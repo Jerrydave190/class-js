ffc3a50c79db62c52fa66a7efbee43716d8f001a
Jerrydave190 <09022562397dave@gmail.com>
2022-08-26 12:45:45 +0100

    modifying pascal_t.js

--- pascal_t.js
+++ pascal_t.js
@@ -1,3 +1,5 @@
+var center = require('center-align');
+
 function factorial(n) {
     if (n == 0) {
         return 1
@@ -5,4 +7,24 @@ function factorial(n) {
         return n * factorial (n-1)
     }
 }
-console.log(factorial(10))
+
+function combination(n, r) {
+    let c_val= factorial(n) / (factorial(n-r) * factorial(r));
+    return c_val
+}
+
+function pascal_t(n){
+    let my_str="";
+    let val=0;
+    for (let j = 0; j <= n; j++) {
+        val=combination(n,j);
+        my_str +=val.toString() + "     ";
+    }
+    return my_str
+}
+console.log(" Printing my pascal triangle    ");
+
+for (let i = 0; i <= 8; i++) {
+    //center(val, width);
+    console.log(center(pascal_t(i), 100));
+}
